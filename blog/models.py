f489d706bbf2772c44464d5e2672a19467e311b4
CristoferNava <cj.carmonanava@ugto.mx>
2020-12-05 21:12:29 -0600

    Ready to add pagination

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Each Django model has a least one manager to handle the data base, the default
+# manager is called objects. 
+# we can create custom managers 
+
+# Manager to retrieve all posts with the published status
+# Post.published.all()
+class PublishedManager(models.Manager):
+    # this method returns the QuerySet that will be executed.
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+                                            .filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -26,3 +39,16 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+    objects = models.Manager() # the default manager
+    published = PublishedManager() # Our custom manager.
+
+    # get_absolute_url use to return a cononical URL for the object.
+    # the reverse() method build URLs by their name and pass optional parameters
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                      args=[self.publish.year,
+                            self.publish.month,
+                            self.publish.day,
+                            self.slug])
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    # this function retrieves the object that matches the given parameters or an
+    # HTTP 404 exception
+    post = get_object_or_404(Post, slug=post, 
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
