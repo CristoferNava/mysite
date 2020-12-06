7c324dca096486a9beae582f996f3a5f27856c2b
CristoferNava <cj.carmonanava@ugto.mx>
2020-12-06 13:45:15 -0600

    CommentForm was added

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25) # rendered as <input type="text">
@@ -7,3 +8,14 @@ class EmailPostForm(forms.Form):
     comments = forms.CharField(required=False, widget=forms.Textarea)
     # Each field has a default widget that determines how the field is rendered
     # in HTML
+
+class CommentForm(forms.ModelForm):
+    # To create a form from a model, we just need to indicate which model to use
+    # to build the form in the Meta class of the form. Django introspects the model
+    # and builds the form dynamically for us.
+    class Meta:
+        model = Comment
+        # By default, Django builds a form field for each field contained in the model.
+        # However, we can explicitly tell the framewoek which fields we want to include
+        # in the form using fields 
+        fields = ('name', 'email', 'body')
--- blog/models.py
+++ blog/models.py
@@ -52,3 +52,25 @@ class Post(models.Model):
                             self.publish.day,
                             self.slug])
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    # each comment will be made on one post, and each post may have multiple comments
+    # the related_name attribute allows you to name the attribute that you use
+    # for the relationship from the related object back to this one
+    # after defining this, we can retrieve the post of a comment object using comment.post
+    # and also retrieve all comments of a post using post.comments.all()
+    name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=80)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} of {self.post}'
+
+
